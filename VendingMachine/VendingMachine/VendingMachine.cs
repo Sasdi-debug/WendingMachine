025ebc05d6e357214769b73eb5b240969f57d382
sasdi_andras_zeno <sasdi.andras.zeno@C116-04>
2026-09-25 14:20:59 +0200

    Added products.txt

---
+++ VendingMachine/VendingMachine/VendingMachine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    public class VendingMachine
+    {
+        private List<Product> products = new();
+        private int _balance;
+        private int _income;
+        public int Balance { get { return _balance; } }
+        public int Income { get { return _income; } }
+        
+    }
+}
