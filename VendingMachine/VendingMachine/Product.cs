ff3742221c1e834d53668e1e6aa981f907c89eb1
sasdi_andras_zeno <sasdi.andras.zeno@C116-04>
2026-09-25 13:48:12 +0200

    Added code, name, price, stock, category fields and properties.

---
+++ VendingMachine/VendingMachine/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    public class Product
+    {
+        private string _code;
+        private string _name;
+        private int _price;
+        private int _stock;
+        private ProductCategory _category;
+
+        public string Code { get { return _code; } }
+        public string Name { get { return _name; } }
+        public int Price { get { return _price; } }
+        public int Stock { get { return _stock; } }
+        public ProductCategory Category { get { return _category; } }
+
+    }
+}
